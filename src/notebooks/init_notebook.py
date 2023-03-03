f9418e7e487aa077adbc5536a03e7e5213d6b2c1
adhocmaster <adhocmaster@live.com>
2023-03-03 10:06:54 -0800

    Incomplete trajectores cannot start from 0. Need to re-label them and exclude them from analysis

--- src/notebooks/init_notebook.py
+++ src/notebooks/init_notebook.py
@@ -6,7 +6,7 @@ except ValueError: # Already removed
     pass
 
 projectFolder = 'E:\\AV\\DataSetExploration\\TTI-dataset-tools\\src'
-# projectFolder = 'D:/AV/DataSetExploration/TTI-dataset-tools/src'
+projectFolder = 'D:/AV/DataSetExploration/TTI-dataset-tools/src'
 sys.path.append(str(projectFolder))
 os.chdir(projectFolder)
 print( f"current working dir{os.getcwd()}")
